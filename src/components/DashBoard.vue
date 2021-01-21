8ddaeb290f0de7bfeded23712f5a78b68b4eed25
oggong <49095304+oggong@users.noreply.github.com>
2021-01-21 20:38:13 +0900

    vue.js firebase test

---
+++ src/components/DashBoard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="dashboard">
+        <h3>DashBoard</h3>
+    </div>
+</template>
+<script>
+export default {
+    name: 'dashboard',
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
---
+++ src/components/NewEmployee.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="new-employee">
+        <h3>NewEmployee</h3>
+    </div>
+</template>
+<script>
+export default {
+    name: 'new-employee',
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
