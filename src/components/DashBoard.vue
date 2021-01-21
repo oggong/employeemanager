5cfa2e3af78a878c60cdcaf59b5be919b452ed52
oggong <49095304+oggong@users.noreply.github.com>
2021-01-21 22:02:36 +0900

    보류

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -9,12 +9,26 @@
     </div>
 </template>
 <script>
+import db from './firebaseInit';
+
 export default {
     name: 'dashboard',
     data() {
         return {
-
+            employees: []
         }
+    },
+    created(){
+        console.log(db.collection('employees').get())
+        // db.collection('employees').get().then(querySnapshot => {
+        //     querySnapshot.forEach(doc => {
+        //         console.log(doc);
+
+        //         const data = {
+            
+        //         }
+        //     });
+        // })
     }
 }
 </script>
