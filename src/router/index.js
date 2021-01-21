24b081e7c4a13bc7655da9243a7949432116052f
oggong <49095304+oggong@users.noreply.github.com>
2021-01-21 20:52:17 +0900

    structure init

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,34 @@
 import Vue from "vue";
+import Router from "vue-router";
+
+import DashBoard from "@/components/DashBoard.vue";
+import NewEmployee from "@/components/NewEmployee";
+import ViewEmployee from "@/components/ViewEmployee";
+import EditEmployee from "@/components/EditEmployee";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "dashboard",
+      component: DashBoard,
+    },
+    {
+      path: "/new",
+      name: "new-employee",
+      component: NewEmployee,
+    },
+    {
+      path: "/edit/:employee_id",
+      name: "edit-employee",
+      component: EditEmployee,
+    },
+    {
+      path: "/:employee_id",
+      name: "view-employee",
+      component: ViewEmployee,
+    },
+  ],
+});
